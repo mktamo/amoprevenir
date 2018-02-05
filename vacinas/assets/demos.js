d1afdd375eb5c269eec3ad159d0b43b59a7bbc27
mktamo <35562505+mktamo@users.noreply.github.com>
2018-02-05 08:38:01 -0200

    Update demos.js

--- vacinas/assets/demos.js
+++ vacinas/assets/demos.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
  
   new Typed('#typed3', {
-    strings: ['Encontre tudo sobre<br>vacinas contra <span style="font-family: 'Open Sans bold'">BCG</span>', 'Encontre tudo sobre <br>vacinas contra <span style="font-family: 'Open Sans bold'">Hepatite A</span>', 'Encontre tudo sobre <br>vacinas contra <span style="font-family: 'Open Sans bold'">Hepatite B</span>', 'Encontre tudo sobre <br>vacinas contra <span style="font-family: 'Open Sans bold'">Meningocócica C conjugada</span>', 'Encontre tudo sobre <br>vacinas contra <span style="font-family: 'Open Sans bold'">Febre Amarela</span>', 'Encontre tudo sobre <br>vacinas contra <span style="font-family: 'Open Sans bold'">Febre Amarela</span>', 'E muito mais!'],
+    strings: ['Encontre tudo sobre<br>vacinas contra <span style="font-family: \'Open Sans bold\'">BCG</span>', 'Encontre tudo sobre <br>vacinas contra <span style="font-family: \'Open Sans bold\'">Hepatite A</span>', 'Encontre tudo sobre <br>vacinas contra <span style="font-family: \'Open Sans bold\'">Hepatite B</span>', 'Encontre tudo sobre <br>vacinas contra <span style="font-family: \'Open Sans bold\'">Meningocócica C conjugada</span>', 'Encontre tudo sobre <br>vacinas contra <span style="font-family: \'Open Sans bold\'">Febre Amarela</span>', 'Encontre tudo sobre <br>vacinas contra <span style="font-family: \'Open Sans bold\'">Febre Amarela</span>', 'E muito mais!'],
     typeSpeed: 1,
     backSpeed: 1,
     smartBackspace: true,
